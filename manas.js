c3b743cf39d54d5382ba38801333277913f8af02
gau <arietmatasev7@gmail.com>
2025-12-26 15:36:37 +0300

    Update and rename manas.js to t

--- manas.js
+++
@@ -1,9 +0,0 @@
-// Функция для показа дополнительного текста в разделе "Статья 5"
-function showText() {
-    const extraText = document.getElementById("extra-text");
-    if (extraText.style.display === "none") {
-      extraText.style.display = "block";
-    } else {
-      extraText.style.display = "none";
-    }
-  }
